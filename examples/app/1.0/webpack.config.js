77f55e309649f8645e0b2abae931c55d1d75ff92
wood1986 <5212215+wood1986@users.noreply.github.com>
2021-12-27 14:53:55 -0800

    feat: support setupMiddlewares

--- examples/app/1.0/webpack.config.js
+++ examples/app/1.0/webpack.config.js
@@ -14,15 +14,20 @@ module.exports = (env, argv) => {
 
   config.devServer = {
     ...config.devServer,
-    "onAfterSetupMiddleware": (devServer) => {
-      devServer.app.get("*", ReactSSRMiddleware(
-        devServer.compiler,
-        {
-          "reqToProps": (req) => ({"url": url.parse(req.originalUrl, true)}),
-          version,
-        }
-      ));
-    },
+    "setupMiddlewares": (middlewares, devServer) => ([
+      ...middlewares,
+      {
+        "name": "ReactSSRWebpackPlugin",
+        "path": "*",
+        "middleware": ReactSSRMiddleware(
+          devServer.compiler,
+          {
+            "reqToProps": (req) => ({"url": url.parse(req.originalUrl, true)}),
+            version,
+          }
+        ),
+      },
+    ]),
     "open": ["/a.node"],
   };
 
