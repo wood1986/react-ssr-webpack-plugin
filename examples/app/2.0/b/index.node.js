b76724335e0ae849d5195e1138b77895dadb7b7d
ATNASGDWNGTH <5212215+wood1986@users.noreply.github.com>
2022-02-05 23:50:53 -0800

    fix: fix path and crossOrigin in app/2.0 (#28)

--- examples/app/2.0/b/index.node.js
+++ examples/app/2.0/b/index.node.js
@@ -18,14 +18,14 @@ export default async (props = {}) => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=" />
-      <link integrity={__DIGESTS__["b.web.css"]} rel="stylesheet" href={`${__webpack_public_path__}/${__FILES__["b.web.css"]}`} />
+      <link integrity={__DIGESTS__["b.web.css"]} rel="stylesheet" href={`${__webpack_public_path__}${__FILES__["b.web.css"]}`} crossOrigin="anonymous" />
       <script dangerouslySetInnerHTML={{"__html": `globalThis.props = ${JSON.stringify(props).replace(/</g, "\\u003c")}`}} />
       <script dangerouslySetInnerHTML={{"__html": __SOURCES__["b.web.js"]}} />
       {sheet.getStyleElement()}
     </head>
     <body>
       <div id="root" dangerouslySetInnerHTML={{"__html": div}} />
-      <script integrity={__DIGESTS__["vendors.js"]} src={`${__webpack_public_path__}/${__FILES__["vendors.js"]}`} crossOrigin="anonymous" />
+      <script integrity={__DIGESTS__["vendors.js"]} src={`${__webpack_public_path__}${__FILES__["vendors.js"]}`} crossOrigin="anonymous" />
     </body>
   </html>);
 
